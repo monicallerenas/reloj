0cc5effa24b5d7c73e3ed4ea1a57d07b67c3011a
monicallerenas <kmonik28@gmail.com>
2016-06-16 13:46:46 -0500

    recargar para actualizar fecha

--- js/main.js
+++ js/main.js
@@ -1,88 +1,76 @@
 function mueveReloj(){ 
+
     momentoActual = new Date() 
     hora = momentoActual.getHours() 
     minuto = momentoActual.getMinutes() 
     segundo = momentoActual.getSeconds() 
 
     str_segundo = new String (segundo) 
-        if (str_segundo.length == 1) 
+    
+    if (str_segundo.length == 1) 
         segundo = "0" + segundo 
 
-      str_minuto = new String (minuto) 
-        if (str_minuto.length == 1) 
+    str_minuto = new String (minuto) 
+    if (str_minuto.length == 1) 
         minuto = "0" + minuto 
 
-      str_hora = new String (hora) 
-        if (str_hora.length == 1) 
+    str_hora = new String (hora) 
+    if (str_hora.length == 1) 
         hora = "0" + hora 
 
     horaImprimible = hora + " : " + minuto + " : " + segundo 
 
     document.form_reloj.reloj.value = horaImprimible 
 
-    setTimeout("mueveReloj()",1000)
+    
     onfocus="window.document.form_reloj.reloj.blur()" 
-//funcion para el dia
-
-    /*var dia= new ("","Lunes", "Martes", "Miercoles","Jueves","Viernes", "Sabado", "Domingo");
-    var mes= new ("", "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre");
-
-    dateText+=dia[dayValue];
-    dateText+=mes[monthValue];
-*/
-
-
-
-  /*  dayValue = A.getDay()
-if (dayValue == 0)
-dateText += "Domingo"
-else if (dayValue == 1)
-dateText += "Lunes"
-else if (dayValue == 2)
-dateText += "Martes"
-else if (dayValue == 3)
-dateText += "Miercoles"
-else if (dayValue == 4)
-dateText += "Jueves"
-else if (dayValue == 5)
-dateText += "Viernes"
-else if (dayValue == 6)
-dateText += "Sabado"
-
-monthValue = A.getMonth()
-dateText += " "
-if (monthValue == 0)
-dateText += "Enero"
-if (monthValue == 1)
-dateText += "Febrero"
-if (monthValue == 2)
-dateText += "Marzo"
-if (monthValue == 3)
-dateText += "Abril"
-if (monthValue == 4)
-dateText += "Mayo"
-if (monthValue == 5)
-dateText += "Junio"
-if (monthValue == 6)
-dateText += "Julio"
-if (monthValue == 7)
-dateText += "Agosto"
-if (monthValue == 8)
-dateText += "Septiembre"
-if (monthValue == 9)
-dateText += "Octubre"
-if (monthValue == 10)
-dateText += "Noviembre"
-if (monthValue == 11)
-dateText += "Diciembre"
-
-
-*/
     var n=0;
-    function selecionapais(obj){
-    n=obj.options[obj.selectedIndex].value;
-    alert(selecionapais ('Lima', '+5'));
-    alert(selecionapais ('Mexico', '-5'));
-  //tufunction(n);
+
+
+   
 }
-} 
+
+ function selecionapais(){
+        console.log(this)
+        var texto = '';
+
+        var d=new Date();
+        var dia=new Array(7);
+        dia[0]=" Domingo";
+        dia[1]=" Lunes";
+        dia[2]=" Martes";
+        dia[3]=" Miercoles";
+        dia[4]=" Jueves";
+        dia[5]=" Viernes";
+        dia[6]=" Sabado";
+        texto += "Hoy es:" +dia[d.getDay()];
+      
+        var mm=new Date();
+        var m2 = mm.getMonth() + 1;
+        var mesok = (m2 < 10) ? '0' + m2 : m2;
+        var mesok=new Array(12);
+        mesok[0]=" Enero";
+        mesok[1]=" Febrero";
+        mesok[2]=" Marzo";
+        mesok[3]=" Abril";
+        mesok[4]=" Mayo";
+        mesok[5]=" Junio";
+        mesok[6]=" Julio";
+        mesok[7]=" Agosto";
+        mesok[8]=" Septiembre";
+        mesok[9]=" Octubre";
+        mesok[10]=" Noviembre";
+        mesok[11]=" Diciembre";
+        texto +=  "" + mesok[mm.getMonth()];
+
+        var  today = new Date();
+        var m = today.getMonth() + 1;
+        var mes = (m < 10) ? '0' + m : m;
+
+        texto += ''+today.getDate(),'/'+mes,'/'+today.getYear();
+        document.getElementById('phora').innerHTML = texto;
+        
+    }
+
+
+//selecionapais();
